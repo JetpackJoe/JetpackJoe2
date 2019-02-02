ece3376b918701df2a92ff5d318001c5f0bb266e
John Jelatis <JetpackJoe>
2019-02-02 16:11:57 -0600

    Changes

--- js/init.js
+++ js/init.js
@@ -1,8 +1,10 @@
 import {Game} from './game.js';
 window.addEventListener("DOMContentLoaded", ()=>{
 	let game = new Game();
-	game.init().then(()=>{
+	game.init().then(g=>{
 		document.querySelector('#noscript').style.display='none';
 		document.body.innerText = JSON.stringify(game.levels);
+	}).catch(err => {
+		alert(err);
 	});
 });
