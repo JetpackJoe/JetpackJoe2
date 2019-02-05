65b2f6d62e4c4b4157fef47a37a0c03e1f493b03
John Jelatis <JetpackJoe>
2019-02-04 23:59:49 -0600

    Changes

--- js/level.js
+++ js/level.js
@@ -19,12 +19,14 @@ export class Level {
 		});
 	}
 	drawOn(ctx, playerX = 0, ss = this.ss) {
+		// use 1% screheight as a unit
+		let h = ctx.canvas.height;
 		for(let block of this.json.blocks) {
 			let drawAt = [
-				ctx.canvas.height*(block[0])/100 - ctx.canvas.width*playerX/100,
-				ctx.canvas.height*(block[1])/100,
-				ctx.canvas.height*(block[2])/100,
-				ctx.canvas.height*(block[3])/100
+				h * (block[0] - playerX) / 100,
+				h * (block[1]) / 100,
+				h * (block[2]) / 100,
+				h * (block[3]) / 100
 			];
 			if(block[4].includes(':')) {
 				ss.draw(block[4], ctx, ...drawAt)
@@ -32,11 +34,27 @@ export class Level {
 				let colour = new Color(block[4] || '#000');
 				ctx.fillStyle = colour.darken(0x0F);
 				ctx.strokeStyle = colour.lighten(0x1F);
-				ctx.lineWidth = ctx.canvas.height * 0.005;
+				ctx.lineWidth = h * 0.005;
 				ctx.rect(...drawAt);
-				ctx.fill(); // fill it up
+				ctx.fill(); // fill it with colour
 				ctx.stroke(); // uhh well you see.. it's having a stroke
 			}
 		}
+	}
+	checkForCollisions(entity) {
+		for(let plat of this.json.blocks) {
+			if(entity.pos.x < this.json.blocks[0] + this.json.blocks[2] && 
+				entity.pos.x > this.json.blocks[0]) {
+				// then it's within the same X-general-area
+				if(entity.pos.y > this.json.blocks[1] &&
+					entity.pos.y < this.json.blocks[1]+this.json.blocks[3]) {
+					// MAY DAY!!
+					// may the day.. come when you'll crash?
+					// sure
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
